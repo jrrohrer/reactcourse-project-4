6f24badde56f7cf6754b4489f23c42f16a5ed125
Jessie Rohrer <35381492+jrrohrer@users.noreply.github.com>
2025-03-07 12:39:36 -0500

    it's the whole thing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <TipCalculator />
     </div>
   );
 }
 
-export default App;
+function TipCalculator() {
+  const [bill, setBill] = useState("");
+  const [tip, setTip] = useState(0);
+
+  const tipAmount = bill * (tip / 100);
+
+  function handleReset() {
+    setBill("");
+    setTip(0);
+  }
+
+  return (
+    <>
+      <BillInput bill={bill} onSetBill={setBill} />
+      <TipInput
+        msg="How much would you like to tip?"
+        tip={tip}
+        onSelect={setTip}
+      />
+
+      {bill > 0 && (
+        <>
+          <Total bill={bill} tip={tipAmount} />
+          <Reset onReset={handleReset} />
+        </>
+      )}
+    </>
+  );
+}
+
+function BillInput({ bill, onSetBill }) {
+  return (
+    <>
+      <label>Bill Amount:</label>
+      <input
+        type="number"
+        value={bill}
+        onChange={(e) => onSetBill(Number(e.target.value))}
+      />
+    </>
+  );
+}
+
+function TipInput({ msg, tip, onSelect }) {
+  return (
+    <>
+      <label>{msg}</label>
+      <select value={tip} onChange={(e) => onSelect(Number(e.target.value))}>
+        <option value="0">0%</option>
+        <option value="5">5%</option>
+        <option value="10">10%</option>
+        <option value="15">15%</option>
+        <option value="20">20%</option>
+      </select>
+    </>
+  );
+}
+
+function Total({ bill, tip }) {
+  const total = (bill + tip).toFixed(2);
+  const billFinal = bill.toFixed(2);
+  const tipFinal = tip.toFixed(2);
+
+  return (
+    <p>
+      Total: ${total} (${billFinal} + ${tipFinal} tip)
+    </p>
+  );
+}
+
+function Reset({ onReset }) {
+  return <button onClick={onReset}>Reset</button>;
+}
